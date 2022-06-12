5a0c203bd2d30a93b06e578e476fdcb75686cef7
Marine Master <85358481+marine-master@users.noreply.github.com>
2022-06-12 23:35:39 +0800

    graceful shutdown when running in console in Windows and user click "X" button (close window)

--- org.freenetproject.ored.support/src/main/java/freenet/support/node/SemiOrderedShutdownHook.java
+++ org.freenetproject.ored.support/src/main/java/freenet/support/node/SemiOrderedShutdownHook.java
@@ -1,12 +1,33 @@
+/*
+ * Copyright 1999-2022 The Freenet Project
+ * Copyright 2022 Marine Master
+ *
+ * This file is part of Oldenet.
+ *
+ * Oldenet is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or any later version.
+ *
+ * Oldenet is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Oldenet.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package freenet.support.node;
 
-import static java.util.concurrent.TimeUnit.SECONDS;
-
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
-public class SemiOrderedShutdownHook extends Thread {
+import com.sun.jna.Native;
+import com.sun.jna.Platform;
+import com.sun.jna.win32.StdCallLibrary;
 
-	private static final long TIMEOUT = SECONDS.toMillis(100);
+public final class SemiOrderedShutdownHook extends Thread {
+
+	private static final long TIMEOUT = TimeUnit.SECONDS.toMillis(100);
 
 	private final ArrayList<Thread> earlyJobs;
 
@@ -16,6 +37,41 @@ public class SemiOrderedShutdownHook extends Thread {
 
 	static {
 		Runtime.getRuntime().addShutdownHook(singleton);
+
+		if (Platform.isWindows()) {
+			var ret = Kernel32Ex.INSTANCE.SetConsoleCtrlHandler((dwCtrlType) -> {
+				if (dwCtrlType == 2 || dwCtrlType == 5 || dwCtrlType == 6) {
+					try {
+						singleton.start();
+					}
+					catch (IllegalThreadStateException ignored) {
+						// Already started. Just join.
+					}
+					try {
+						singleton.join();
+					}
+					catch (InterruptedException ignored) {
+						return false;
+					}
+					try {
+						// Sleep for 1 second for users to see shutdown information
+						TimeUnit.SECONDS.sleep(1);
+					}
+					catch (InterruptedException ignored) {
+
+					}
+					return true;
+				}
+				return false;
+			}, true);
+
+			if (ret) {
+				System.out.println("SetConsoleCtrlHandler registered");
+			}
+			else {
+				System.out.println("SetConsoleCtrlHandler failed to register");
+			}
+		}
 	}
 
 	public static SemiOrderedShutdownHook get() {
@@ -23,16 +79,16 @@ public class SemiOrderedShutdownHook extends Thread {
 	}
 
 	private SemiOrderedShutdownHook() {
-		earlyJobs = new ArrayList<Thread>();
-		lateJobs = new ArrayList<Thread>();
+		this.earlyJobs = new ArrayList<>();
+		this.lateJobs = new ArrayList<>();
 	}
 
 	public synchronized void addEarlyJob(Thread r) {
-		earlyJobs.add(r);
+		this.earlyJobs.add(r);
 	}
 
 	public synchronized void addLateJob(Thread r) {
-		lateJobs.add(r);
+		this.lateJobs.add(r);
 	}
 
 	@Override
@@ -40,7 +96,7 @@ public class SemiOrderedShutdownHook extends Thread {
 		System.err.println("Shutting down...");
 		// First run early jobs, all at once, and wait for them to all complete.
 
-		Thread[] early = getEarlyJobs();
+		Thread[] early = this.getEarlyJobs();
 
 		for (Thread r : early) {
 			r.start();
@@ -49,13 +105,13 @@ public class SemiOrderedShutdownHook extends Thread {
 			try {
 				r.join(TIMEOUT);
 			}
-			catch (InterruptedException e) {
+			catch (InterruptedException ignored) {
 				// :(
 				// May as well move on
 			}
 		}
 
-		Thread[] late = getLateJobs();
+		Thread[] late = this.getLateJobs();
 
 		// Then run late jobs, all at once, and wait for them to all complete (JVM will
 		// exit when we return).
@@ -66,7 +122,7 @@ public class SemiOrderedShutdownHook extends Thread {
 			try {
 				r.join(TIMEOUT);
 			}
-			catch (InterruptedException e) {
+			catch (InterruptedException ignored) {
 				// :(
 				// May as well move on
 			}
@@ -75,11 +131,25 @@ public class SemiOrderedShutdownHook extends Thread {
 	}
 
 	private synchronized Thread[] getEarlyJobs() {
-		return earlyJobs.toArray(new Thread[earlyJobs.size()]);
+		return this.earlyJobs.toArray(new Thread[0]);
 	}
 
 	private synchronized Thread[] getLateJobs() {
-		return lateJobs.toArray(new Thread[lateJobs.size()]);
+		return this.lateJobs.toArray(new Thread[0]);
+	}
+
+	interface Kernel32Ex extends StdCallLibrary {
+
+		Kernel32Ex INSTANCE = Native.load("kernel32", Kernel32Ex.class);
+
+		boolean SetConsoleCtrlHandler(HANDLER_ROUTINE handler, boolean add);
+
+		interface HANDLER_ROUTINE extends StdCallLibrary.StdCallCallback {
+
+			boolean callback(int dwCtrlType);
+
+		}
+
 	}
 
 }
