a3dc7689f58c6f41cc38f356c33a72534b3f4ce7
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2022-12-31 10:03:46 +0100

    ✅ Improve test for insert compressor metrics
    The previous test only matched the number of results. Here I added a Hamcrest
    matcher for the actual results, making this test way more expressive.

--- test/freenet/client/async/InsertCompressorTest.java
+++ test/freenet/client/async/InsertCompressorTest.java
@@ -1,28 +1,26 @@
 package freenet.client.async;
 
-import java.io.IOException;
 import java.io.OutputStream;
 
 import freenet.client.InsertContext;
-import freenet.client.InsertException;
 import freenet.client.async.InsertCompressor.CompressionMetrics;
+import freenet.client.async.InsertCompressor.CompressionMetrics.Run;
 import freenet.config.Config;
-import freenet.config.InvalidConfigValueException;
-import freenet.config.NodeNeedRestartException;
 import freenet.config.SubConfig;
 import freenet.support.Executor;
 import freenet.support.api.BucketFactory;
 import freenet.support.api.IntCallback;
 import freenet.support.api.RandomAccessBucket;
-import freenet.support.compress.Compressor.COMPRESSOR_TYPE;
-import freenet.support.compress.InvalidCompressionCodecException;
 import freenet.support.io.ArrayBucketFactory;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeDiagnosingMatcher;
 import org.junit.Test;
 
 import static freenet.client.InsertContext.CompatibilityMode.COMPAT_DEFAULT;
 import static freenet.support.io.BucketTools.toRandomAccessBucket;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 
 public class InsertCompressorTest {
 
@@ -33,7 +31,7 @@ public class InsertCompressorTest {
 	}
 
 	@Test
-	public void startingACompressorIsCounted() throws InsertException, IOException, NodeNeedRestartException, InvalidConfigValueException, InvalidCompressionCodecException {
+	public void compressionStatisticsAreCorrect() throws Exception {
 		SingleFileInserter inserter = createSingleFileInserter();
 		BucketFactory bucketFactory = new ArrayBucketFactory();
 		RandomAccessBucket originalData = toRandomAccessBucket(bucketFactory.makeBucket(1024), bucketFactory);
@@ -50,7 +48,40 @@ public class InsertCompressorTest {
 		Executor mainExecutor = createNullExecutor();
 		ClientContext clientContext = new ClientContext(0, null, mainExecutor, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, config);
 		insertCompressor.tryCompress(clientContext);
-		assertThat(compressionMetrics.getRuns(), hasSize(COMPRESSOR_TYPE.getCompressorsArray(null).length));
+		/* this needs to be adjusted every time we change compression algorithms. */
+		assertThat(compressionMetrics.getRuns(), containsInAnyOrder(
+				matchesRun("GZIP", 1024, 29),
+				matchesRun("BZIP2", 1024, 40),
+				matchesRun("LZMA_NEW", 1024, 23)
+		));
+	}
+
+	private static Matcher<Run> matchesRun(String algorithm, long sizeBeforeCompression, long sizeAfterCompression) {
+		return new TypeSafeDiagnosingMatcher<Run>() {
+			@Override
+			protected boolean matchesSafely(Run run, Description mismatchDescription) {
+				if (!run.algorithm.equals(algorithm)) {
+					mismatchDescription.appendText("algorithm is ").appendValue(run.algorithm);
+					return false;
+				}
+				if (run.sizeBeforeCompression != sizeBeforeCompression) {
+					mismatchDescription.appendText("sizeBeforeCompression is ").appendValue(run.sizeBeforeCompression);
+					return false;
+				}
+				if (run.sizeAfterCompression != sizeAfterCompression) {
+					mismatchDescription.appendText("sizeAfterCompression is ").appendValue(run.sizeAfterCompression);
+					return false;
+				}
+				return true;
+			}
+
+			@Override
+			public void describeTo(Description description) {
+				description.appendText("run with algorithm ").appendValue(algorithm)
+						.appendText(", sizeBeforeCompression ").appendValue(sizeBeforeCompression)
+						.appendText(", sizeAfterCompression ").appendValue(sizeAfterCompression);
+			}
+		};
 	}
 
 	private static SingleFileInserter createSingleFileInserter() {
